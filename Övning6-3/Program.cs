cd82dc5e777f75f0be6c1250a38a3c36fcd2575f
erik suserud <eriksuserud@gmail.com>
2019-09-18 15:14:36 +0200

    Övning 6-3

---
+++ Övning6-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Övning6_3
+{
+    class Program
+    {   
+        /*
+        Kontrollera värdet (del 3)
+-	Arbeta vidare på övningen. 
+Gör så att om användaren matar in något annat än ”J”, ”j”, ”n”, ”N” så skriver programmet ut ”Jag förstår inte!”.
+        
+         */
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Är det fint väder ute? J/N");
+            string weather = Console.ReadLine();
+
+            if(weather == "J" || weather == "j")
+            {
+                System.Console.WriteLine("Vi går på picknick");
+
+            }
+            else if(weather == "N" || weather == "n" )
+            {
+                System.Console.WriteLine("Vi stannar inne");
+            }
+            else
+            {
+                System.Console.WriteLine("Jag förstår inte!");
+            }
+        }
+    }
+}
