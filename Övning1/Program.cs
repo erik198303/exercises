8a6dd8dbc7583cfe146bf467e27b8bb412725e1f
erik suserud <eriksuserud@gmail.com>
2019-09-18 14:29:56 +0200

    övning1

---
+++ Övning1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Övning1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          System.Console.Write("Mata in tal 1: ");
+          string input1 = Console.ReadLine();
+          int num1 = Convert.ToInt32(input1);
+          System.Console.Write("Mata in tal 2: ");
+          string input2 = Console.ReadLine();
+          int num2 = Convert.ToInt32(input2);
+          int sum = num1 + num2;
+          System.Console.WriteLine("Summan är {0}", sum);
+        }
+    }
+}
